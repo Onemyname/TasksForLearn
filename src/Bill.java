490810986e38b6b82ac95557bdcf5049a07ad911
Onemyname <konvvadim@yandex.ru>
2022-03-17 12:39:19 +0300

    Create Bill.java

---
+++ src/Bill.java
@@ -0,0 +1,21 @@
+
+/*
+Плата по счетам
+*/
+
+public class Bill {
+    public static Integer balance = Integer.MAX_VALUE;
+
+    public static void main(String[] args) {
+        String bill = "1234567890";
+
+        System.out.println("Текущий баланс : " + balance);
+        processPayment(bill);
+        System.out.println("Текущий баланс : " + balance);
+    }
+
+    public static void processPayment(String bill) {
+        balance = balance - Integer.parseInt(bill);
+    }
+}
+

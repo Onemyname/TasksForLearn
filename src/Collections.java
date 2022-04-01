6705a6b66e68dcd1764de73781b39824d4f7504f
Onemyname <konvvadim@yandex.ru>
2022-04-01 09:04:30 +0300

    Create Collections.java

---
+++ src/Collections.java
@@ -0,0 +1,22 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/*
+Изучаем методы класса Collections, часть 1
+*/
+
+public class Collections {
+
+    public static void copy(ArrayList<String> destination, ArrayList<String> source) {
+        Collections.copy(destination, source);
+    }
+
+    public static void addAll(ArrayList<String> list, String... strings) {
+        Collections.addAll(list, strings);
+    }
+
+    public static void replaceAll(ArrayList<String> list, String oldValue, String newValue) {
+        Collections.replaceAll(list, oldValue, newValue);
+    }
+}

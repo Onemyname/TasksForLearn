028e0e9900a325a2f5a2825d8a56ae2ecb05e626
Onemyname <konvvadim.yandex.ru>
2022-10-15 15:01:33 +0400

    Create few simple inner classes

--- src/Car.java
+++ src/Car.java
@@ -1,18 +1,70 @@
 public class Car {
-    public static void main(String[] args) {
-        Car sportCar = new Car();
+
+    Door door1;
+    Door door2;
+    Door door3;
+    Door door4;
+
+    Engine engine;
+
+    public double getHeight() {
+        return height;
     }
 
-    public Car() {
-Door door = new Door();
+    private double height;
+
+    public static void main(String[] args) {
+        Car sportCar = new Car(1.5);
+        String heightCar = "Высота машины = ";
+        String heightDoor = "Высота двери автомобиля № 1 = ";
+        String enginePower = "Количество лошадиных сил двигателя = ";
+        System.out.println(heightCar + sportCar.getHeight() + " м");
+        System.out.println(heightDoor + sportCar.door1.getDoorHeight() + " м");
+        System.out.println(enginePower + sportCar.engine.getHorsePower() + " л/c");
+    }
+
+    public Car(double height) {
+        this.height = height;
+        Door door1 = new Door();
+        Door door2 = new Door();
+        Door door3 = new Door();
+        Door door4 = new Door();
+        this.door1 = door1;
+        this.door2 = door2;
+        this.door3 = door3;
+        this.door4 = door4;
+
+        Engine engine = new Engine(250);
+        this.engine = engine;
         System.out.println("А затем машина!");
     }
 
-    class Door {
-public Door(){
-    System.out.println("Дверь создана!");
-}
+    protected class Door {
+        public double getDoorHeight() {
+            return doorHeight;
+        }
+
+        private double doorHeight;
+
+        public Door() {
+            doorHeight = getHeight() * 0.5;
+            System.out.println("Дверь создана!");
+
+        }
+
     }
 
+    protected class Engine {
+        public int getHorsePower() {
+            return horsePower;
+        }
+
+        private int horsePower;
+
+        public Engine(int horsePower) {
+            System.out.println("Двигатель создан!");
+            this.horsePower = horsePower;
+        }
+    }
 
 }
